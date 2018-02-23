ab64429f4a037bd5f8b899c2d8ccf97052a552c1
Mahdi Hamdan <hamdani0430@gmail.com>
2018-02-22 20:46:40 -0500

    1

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var gemGoal = [];
+
+var gem1Value = [];
+var gem2Value = [];
+var gem3Value = [];
+
+//  Game counters
+var winCounter = 0;
+$("#win-counter").text(winCounter);
+var lossCounter = 0;
+$("#win-counter").text(lossCounter);
+var scoreCount = 0;
+$("#score-counter").text(scoreCounter);
